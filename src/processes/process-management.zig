d17a6f859c751d07a8b2eac179e2ca949f3e4667
sahil-singh1107 <sahilpython@gmail.com>
2026-04-30 21:55:54 +0530

    add process manager

--- src/processes/process-management.zig
+++ src/processes/process-management.zig
@@ -1,50 +1,23 @@
 const std = @import("std");
-const io_mod = @import("../file-handling/io.zig").IO;
+const Task = @import("task.zig").Task;
 
-pub const Action = enum {
-    ENCRYPT,
-    DECRYPT,
-};
+pub const ProcessManagement = struct {
+    queue: std.Io.Queue(*Task),
 
-pub const Task = struct {
-    file_path: []const u8,
-    action: Action,
-
-    pub fn init(file_path: []const u8, action: Action) Task {
-        return Task{
-            .file_path = file_path,
-            .action = action,
-        };
+    pub fn init(self: *ProcessManagement) ProcessManagement {
+        self.queue = std.Io.Queue(*Task);
+        return ProcessManagement{};
     }
 
-    pub fn to_string(self: *Task, buf: []u8) []u8 {
-        const action_str = switch (self.action) {
-            .ENCRYPT => "ENCRYPT",
-            .DECRYPT => "DECRYPT",
-        };
-
-        return try std.fmt.bufPrint(
-            buf,
-            "{s},{s}",
-            .{ self.file_path, action_str },
-        );
+    pub fn submitToQueue(self: *ProcessManagement, io: std.Io, task: *Task) !void {
+        try self.queue.putOne(io, task);
     }
 
-    pub fn from_string(input: []const u8) !Task {
-        const comma_index = std.mem.indexOfScalar(u8, input, ',') orelse {
-            return error.InvalidFormat;
-        };
-
-        const file_path = input[0..comma_index];
-        const action_str = input[comma_index + 1 ..];
-
-        const action = if (std.mem.eql(u8, action_str, "ENCRYPT"))
-            Action.ENCRYPT
-        else if (std.mem.eql(u8, action_str, "DECRYPT"))
-            Action.DECRYPT
-        else
-            return error.InvalidAction;
-
-        return Task.init(file_path, action);
+    pub fn executeTasks(self: *ProcessManagement, io: std.Io) !void {
+        while (try !self.queue.capacity() != 0) {
+            const front_task = try self.queue.getOne(io);
+            var buf = [1024]u8;
+            std.debug.print("executing task : {s}\n", .{front_task.to_string(&buf)});
+        }
     }
 };
